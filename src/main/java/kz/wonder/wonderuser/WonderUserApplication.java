449490d30842d2dcd520849ebe74dd54b9d72322
Dulatbay <adulatbai@bk.ru>
2024-02-28 14:22:23 +0600

    Initial commit

---
+++ src/main/java/kz/wonder/wonderuser/WonderUserApplication.java
@@ -0,0 +1,13 @@
+package kz.wonder.wonderuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonderUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WonderUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/wonder/wonderuser/WonderUserApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.wonder.wonderuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WonderUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
