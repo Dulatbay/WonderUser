e0eb587789f09d66936210fcb316da23d177003f
Dulatbay <adulatbai@bk.ru>
2024-02-29 15:54:37 +0600

    add: docker-compose.yml

--- src/main/java/kz/wonder/wonderuser/WonderUserApplication.java
+++ src/main/java/kz/wonder/wonderuser/WonderUserApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WonderUserApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WonderUserApplication.class, args);
     }
